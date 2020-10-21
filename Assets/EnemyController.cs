61252f6cc325cf1b911c3e32b9ffba181307811e
JongSaewoo <51853003+JongSaewoo@users.noreply.github.com>
2020-10-21 16:17:08 +0900

    Update EnemyController.cs

--- Assets/EnemyController.cs
+++ Assets/EnemyController.cs
@@ -12,11 +12,13 @@ public class EnemyController : MonoBehaviour
     private NavMeshAgent navMeshAgent;
     private Status status;
     private Transform target;
+    private EnemySpawner enemySpawner;
 
-    public void Setup(Transform target)
+    public void Setup(EnemySpawner enemySpawner, Transform target)
     {
         navMeshAgent = GetComponent<NavMeshAgent>();
         status = GetComponent<Status>();
+        this.enemySpawner = enemySpawner;
         this.target = target;
 
         StartCoroutine("UpdateMove");
@@ -43,6 +45,7 @@ public class EnemyController : MonoBehaviour
         {
             collider.GetComponent<PlayerController>().TakeDamage(damage);
 
+            enemySpawner.DieEnemy();
             Destroy(this.gameObject);
         }
     }
@@ -57,6 +60,7 @@ public class EnemyController : MonoBehaviour
         {
             StopCoroutine("UpdateMove");
 
+            enemySpawner.DieEnemy();
             Destroy(this.gameObject);
         }
     }
