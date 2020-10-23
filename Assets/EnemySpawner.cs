78730d07418da8fc97222f49c5fc4fe44309f9cd
JongSaewoo <51853003+JongSaewoo@users.noreply.github.com>
2020-10-23 10:06:55 +0900

    헤드샷좀비, 좀비모드:버서커 추가

--- Assets/EnemySpawner.cs
+++ Assets/EnemySpawner.cs
@@ -11,6 +11,8 @@ public class EnemySpawner : MonoBehaviour
     [SerializeField]
     private GameObject enemyPrefab;
     [SerializeField]
+    private GameObject enemyHeadShotPrefab;
+    [SerializeField]
     private float spawnTime = 0.5f;
     [SerializeField]
     private int maxEnemyCount = 50;
@@ -35,7 +37,8 @@ public class EnemySpawner : MonoBehaviour
         while (enemyCount < maxEnemyCount)
         {
             int index = Random.Range(0, spawnPoints.Length);
-            GameObject cloneEnemy = Instantiate(enemyPrefab, spawnPoints[index].position, Quaternion.identity);
+            GameObject cloneEnemy = SelectEnemyType(index);
+            // GameObject cloneEnemy = Instantiate(enemyPrefab, spawnPoints[index].position, Quaternion.identity);
 
             cloneEnemy.GetComponent<EnemyController>().Setup(this, target);
 
@@ -55,6 +58,36 @@ public class EnemySpawner : MonoBehaviour
         {
             Debug.Log("Stage Clear");
         }
+    }
+
+    private GameObject SelectEnemyType(int index)
+    {
+        GameObject cloneEnemy;
+
+        int enemyType = Random.Range(0, 100);
+        int enemyMode = Random.Range(0, 100);
+
+        if(enemyType < 70)
+        {
+            // 좀비타입 : 일반좀비(enemy) 등장 확률 70%, HP : 100, 타격 범위 : 전체
+            //            헤드샷좀비(enemyHeadShot) 등장 확률 30%, HP : 50, 타격 범위 : 머리만
+            cloneEnemy = Instantiate(enemyPrefab, spawnPoints[index].position, Quaternion.identity);
+        }
+        else
+        {
+            // 좀비모드 : 일반상태 등장 확률 70%, 이동속도 1, 프리펩 색 : 기본
+            //            버서커(광분)상태 등장 확률 20%, 이동속도 3, 프리펩 색 : 전체 빨간색
+            cloneEnemy = Instantiate(enemyHeadShotPrefab, spawnPoints[index].position, Quaternion.identity);
+        }
+
+        if(enemyMode >= 80)
+        {
+            cloneEnemy.GetComponentInChildren<SkinnedMeshRenderer>().material.color = Color.red;
+            cloneEnemy.GetComponent<Animator>().speed = 3;
+            cloneEnemy.GetComponent<UnityEngine.AI.NavMeshAgent>().speed = 3;
+        }
+
+        return cloneEnemy;
     }
 }
 
