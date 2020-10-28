4b1f7535328806a5dcdd8fd674dffbeffb23a8f0
JongSaewoo <51853003+JongSaewoo@users.noreply.github.com>
2020-10-28 18:05:08 +0900

    DestructibleBarrel(부서지는 장애물) 구현

---
+++ Assets/DestructibleObject.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestructibleObject : InteractionObject
+{
+    [Header("Destructible Object")]
+    [SerializeField]
+    private GameObject destructiblePrefab;
+
+    public override void TakeDamage(int damage)
+    {
+        currentHP -= damage;
+
+        if(currentHP <= 0)
+        {
+            Instantiate(destructiblePrefab, transform.position, transform.rotation);
+
+            Destroy(this.gameObject);
+        }
+    }
+}
